00ba418a47d36fb2e416628e2e65161de97a5e94
Aluno Canoas <aluno.canoas@ensino.ulbra.net>
2026-05-19 22:00:36 -0300

    'vitrine'

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Vitrine  } from './pages/vitrine/vitrine';
+import { Carrinho } from './pages/carrinho/carrinho';
+import { ProdutoDetalhe } from './pages/produto-detalhe/produto-detalhe';
+import { NaoEncontrada } from './pages/nao-encontrada/nao-encontrada';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo:'vitrine',pathMatch:'full'},
+    {path: 'vitrine', component: Vitrine},
+    {path: 'carrinho', component: Carrinho},
+    {path: 'produto-detalhe', component: ProdutoDetalhe},
+    {path: '**', component: NaoEncontrada}   
+];
--- src/app/pages/vitrine/vitrine.ts
+++ src/app/pages/vitrine/vitrine.ts
@@ -1,9 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-vitrine',
-  imports: [],
+  imports: [RouterLink, CommonModule],
   templateUrl: './vitrine.html',
   styleUrl: './vitrine.css',
 })
-export class Vitrine {}
+export class Vitrine {
+  produtos=[
+    {id:1, nome: 'teste1', preco: 1},
+    {id:2, nome: 'teste2', preco: 2},
+    {id:3, nome: 'teste3', preco: 3},
+  ]
+}
